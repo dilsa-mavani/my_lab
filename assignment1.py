417706379422c928f33e541d3532e9803c1e9976
dilsa-mavani <dilsamavani@gmail.com>
2026-01-14 15:52:40 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,74 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("hello atmiya university...")
+hello atmiya university...
+a=input("enter value:")
+enter value:5
+b=input("enter value:")
+enter value:5
+result=int(a)+int(b)
+print(result)
+10
+num=int(input("enter a value:"))
+enter a value:66
+if(num%2==0):
+    print("this number is even.")
+else:
+    print("this number is odd.")
+
+    
+this number is even.
+year=int(input("enter a year:"))
+enter a year:2026
+if(year%4==0 and year%100!=0):
+    print("leap year.")
+else:
+    print("not a leap year.")
+
+    
+not a leap year.
+import math
+print(math.pi)
+3.141592653589793
+a=10
+print(a)
+10
+num=int(input("enter number:"))
+enter number:5
+square=num*num
+print("square=",square)
+square= 25
+import math
+r=float(input("enter radius:"))
+enter radius:77
+area=math.pi * r * r
+print("area of circle=",area)
+area of circle= 18626.502843133883
+a=input("enter anything:")
+enter anything:23
+print(type(a))
+<class 'str'>
+>>> import math
+>>> print("square root:",math.sqrt(16))
+square root: 4.0
+>>> print("power:",math.pow(2,3))
+power: 8.0
+>>> print("PI value:",math.pi)
+PI value: 3.141592653589793
+>>> a=int(input("enter a base number:"))
+enter a base number:56
+>>> b=int(input("enter power:"))
+enter power:6
+>>> print("answer=",a**b)
+answer= 30840979456
+>>> n=int(input("enter number:"))
+enter number:26
+>>> if n > 0:
+...     print("positive number")
+... elif n < 0:
+...     print("nagative number")
+... else:
+...     print("zero")
+... 
+...     
+positive number
