a432adab7f24189dda0e92c78852f9f955de5e1b
dilsa-mavani <dilsamavani@gmail.com>
2026-01-20 18:17:00 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,55 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+p=1000
+r=5
+t=2
+si=(p * r * t) / 100
+print("simple interest=",si)
+simple interest= 100.0
+a=10
+b=20
+if a > b:
+    print("maximum =", a)
+else:
+    print("maximum =", b)
+
+    
+maximum = 20
+for i in range(1, 6):
+    print(i)
+
+    
+1
+2
+3
+4
+5
+s="python"
+print("length =",len(s))
+length = 6
+print("hello world")
+hello world
+>>> s="python"
+>>> print("first character =",s[0])
+first character = p
+>>> s="python"
+>>> print("last character =",s[-1])
+last character = n
+>>> n=-5
+>>> if n > 0:
+...     print("positive number")
+... else:
+...     print("nagative number")
+... 
+...     
+nagative number
+>>> a=10
+>>> b=20
+>>> c=30
+>>> sum=a + b + c
+>>> print("sum =",sum)
+sum = 60
+>>> name=input("enter your name:")
+enter your name:dilsa
+>>> print("Hello",name)
+Hello dilsa
