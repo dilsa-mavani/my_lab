775e010e33ed3665f4f43c0144f9ac4c4b271932
dilsa-mavani <dilsamavani@gmail.com>
2026-01-27 09:02:01 +0530

    Add files via upload

---
+++ operators.py.py
@@ -0,0 +1,65 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #arithmetic operator
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> #assignment operator
+>>> x=15
+>>> print(x)
+15
+>>> #unary minus
+>>> a=5
+>>> print(-a)
+-5
+>>> #relational operator
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> #logical operators
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> #boolean operators
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> #bitwise operators
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a|b)
+7
+>>> #membership operators
+>>> number=[1,2,3,4]
+>>> print(2 in numbers)
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    print(2 in numbers)
+NameError: name 'numbers' is not defined
+>>> print(2 in number)
+True
+>>> print(5 not in number)
+True
+>>> #identity operators
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> 
