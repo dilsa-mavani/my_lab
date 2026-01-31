7b15524d031a9d7d625ad2ccf3c0a15f8233de10
dilsa-mavani <dilsamavani@gmail.com>
2026-01-31 07:54:03 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,11 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #output using print
+>>> print("hello world")
+hello world
+>>> #input using input
+>>> name=input("enter your name:")
+enter your name:python
+>>> print("hello",name)
+hello python
+>>> 
