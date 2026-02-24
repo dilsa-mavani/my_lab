cad4eae280e6008c0374776e7bd76229486416f1
dilsa-mavani <dilsamavani@gmail.com>
2026-02-24 08:12:18 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,63 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+    
+1
+2
+3
+4
+5
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+n=int(input("enter n:"))
+enter n:6
+i=1
+s=0
+while i<=n:
+...     s=s+i
+...     i=i+1
+...     print("sum=",s)
+... 
+...     
+sum= 1
+sum= 3
+sum= 6
+sum= 10
+sum= 15
+sum= 21
+>>> num=int(input("enter number:"))
+enter number:8
+>>> i=1
+>>> while i<=10:
+...     print(num,"x",i,"=",num*i)
+...     i=i+1
+... 
+...     
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+8 x 4 = 32
+8 x 5 = 40
+8 x 6 = 48
+8 x 7 = 56
+8 x 8 = 64
+8 x 9 = 72
+8 x 10 = 80
