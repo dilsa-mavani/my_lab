4ad9aca00b949c6b9d1a577f12520504e1f0ca6e
dilsa-mavani <dilsamavani@gmail.com>
2026-02-24 08:11:55 +0530

    Add files via upload

---
+++ forloop2.py
@@ -0,0 +1,23 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> total=0
+>>> for i in range(1,6):
+...     total=total+i
+...     print("sum is:",total)
+... 
+...     
+sum is: 1
+sum is: 3
+sum is: 6
+sum is: 10
+sum is: 15
+>>> #print list elements
+>>> numbers=[10,20,30,40]
+>>> for n in numbers:
+...     print(n)
+... 
+...     
+10
+20
+30
+40
