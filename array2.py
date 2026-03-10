d5643530c5424e312d81483a93d4762d01488271
dilsa-mavani <dilsamavani@gmail.com>
2026-03-10 09:01:59 +0530

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,17 @@
+#positive indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[1])
+print(arr[4])
+#nagative indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+#modifying elements using index
+from array import array
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
