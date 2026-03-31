a5a1d449aee948e2c48bccebd3820d55af8ad5b2
dilsa-mavani <dilsamavani@gmail.com>
2026-03-31 09:03:19 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,21 @@
+#basic keyword
+def student_info(name,age,city):
+    print("name:",name)
+    print("age:",age)
+    print("city:",city)
+student_info(age=18,city="mumbai",name="raj")    
+#mixing positional and keyword
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(1,c=3,b=2)   
+#practice example
+#using keyword argument
+#create function of simple interest
+def simple_interest(p:float,r:int,t:float):
+    si=(p * r * t)/100
+    print("simple interest:",si)
+simple_interest(p=1000,t=2,r=1.5)
+simple_interest(t=1.5,p=15000,r=2)     
+
