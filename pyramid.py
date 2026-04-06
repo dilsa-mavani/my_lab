0920749a8f0f2e4ee3e91d5d2ccdf08d99ec29ee
dilsa-mavani <dilsamavani@gmail.com>
2026-04-06 17:06:50 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,14 @@
+"""n=int(input("enter number of lines:"))
+i=1
+while i < = i:
+    print("*" * i)
+    i+=1"""
+
+for i in range(1,5):
+    print("*" * i)    
+
+n=5
+for i in range(1,n+1):
+   for i in range(1,i+1):
+    print(i,end="")
+    print()
